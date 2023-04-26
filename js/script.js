2bd593287dfaff03beeb5a57b807650c2796063c
lovish <lovishduggal11@gmail.com>
2023-04-26 14:07:45 +0530

    projectFinished

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // * Intialize slick slider
+let bool = false;
 $(document).ready(function(){
     $('.food-slider').slick({
         autoplay:true,
@@ -6,5 +7,35 @@ $(document).ready(function(){
         slidesToScroll:1,
         nextArrow:'.next-btn',
         prevArrow:'.prev-btn',
+        responsive:[
+            {
+                breakpoint: 992,
+                settings:{
+                    slidesToShow:2,
+                }
+            }
+        ],
+        responsive:[
+            {
+                breakpoint: 768,
+                settings:{
+                    slidesToShow:1,
+                }
+            }
+        ]
     });
+    $('.nav-trigger').click(()=>{
+        $('.site-content-wrapper').toggleClass('scaled');
+        bool = !bool;
+        if(bool)
+        {
+            document.querySelector('.nav-trigger').children[0].src = './icons/icon-close-menu.svg';
+            bool = true;
+        }
+        else
+        {
+            document.querySelector('.nav-trigger').children[0].src = './icons/icon-menu.svg';
+            bool = false;
+        }
+    })
 });
